447bbe8b433279b85b6be94b0e94fa69c6c70f65
Kevin Koontz <contactkkoontz@gmail.com>
2024-08-27 15:02:26 -0600

    cardHTMLTemplate created

--- app/models/snacks.js
+++ app/models/snacks.js
@@ -5,11 +5,20 @@ export class Snack {
     this.imgUrl = imgUrl
   }
 
-
-
-
-
-
-
-
+  get cardHTMLTemplate() {
+    return /*html*/`
+        <div class="col-12 d-flex justify-content-center">
+          <div class="px-5 py-3 vendr-card">
+            <p class="display-1">
+              IMG
+            </p>
+            <div class="bg-dark text-light">
+              <p class="fs-3">NAME</p>
+              <p class="fs-5">PRICE</p>
+              <button class="btn btn-primary p-3 fs-1">BUY HERE</button>
+            </div>
+          </div>
+        </div>
+        `
+  }
 }
